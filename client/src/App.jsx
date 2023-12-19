a03419045192da3d38eed1ee02ba64cf35948cd8
sasikola <86250636+sasikola@users.noreply.github.com>
2023-12-19 12:52:18 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./Components/NavigationBar";
 import User from "./Pages/User";
 import CreateUser from "./Pages/CreateUser";
@@ -13,11 +8,6 @@ import Login from "./Pages/Login";
 import Dashboard from "./Pages/Dashboard";
 
 export default function App() {
- 
-
- 
- 
-
   return (
     <div className="w-full max-h-full bg-gray-500 ">
       <Router>
@@ -26,13 +16,10 @@ export default function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
           <Route path="/user" element={<User />} />
           <Route path="/create" element={<CreateUser />} />
-=======
-          <Route path="/user" element={!isLoggedIn ? <Login/> :<User/>} />
-          <Route path="/create" element={isLoggedIn ? <CreateUser /> : <Login/>} />
->>>>>>> 7d40c80e3f227949387ad3d751d185d34d924d52
+          <Route path="/user" element={<User />} />
+          <Route path="/create" element={<CreateUser />} />
           <Route path="/edit/:id" element={<UpdateUser />} />
         </Routes>
       </Router>
