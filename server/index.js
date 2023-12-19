7e09dbc66d4ee49a351ffa1023c2c7a231f829b2
sasikola <86250636+sasikola@users.noreply.github.com>
2023-12-19 10:59:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 <<<<<<< HEAD
 app.use(
   cors({
-    origin: ["https://deploy-mern-1whq.vercel.app"],
+    origin: ["https://user-mern-client.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
